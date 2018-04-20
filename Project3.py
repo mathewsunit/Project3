bf86580f2e9f879b785e7c8c38af56b71b2f4bb1
Sunit Mathew <mathewsunit@gmail.com>
2018-04-20 00:36:00 -0500

    Final submission

--- Project3.py
+++ Project3.py
@@ -1,40 +1,65 @@
 import os
 import warnings
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.eager as tfe
-
-tfe.enable_eager_execution()
-
-# TensorFlow MNIST
 from tensorflow.examples.tutorials.mnist import input_data
+
+# pre-config
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.simplefilter(action='ignore', category=FutureWarning)
+tfe.enable_eager_execution()
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-dim_hidden = 1024
+dim_hidden = 1028
 
-layer_cnn0 = tf.layers.Conv2D(32, 5, activation=tf.nn.relu)
-layer_pool2x2 = tf.layers.MaxPooling2D(2, 2)
-layer_pool3x3 = tf.layers.MaxPooling2D(3, 3)
+# Layer from prof DONOT CHANGE
 layer_pool4x4 = tf.layers.MaxPooling2D(4, 4)
-layer_cnn1 = tf.layers.Conv2D(64, 5, activation=tf.nn.relu)
+
+layer_cnn0 = tf.layers.Conv2D(
+    filters=8,
+    kernel_size=[5, 5],
+    padding="same",
+    activation=tf.nn.relu,
+    # strides=[2,2]
+)
+layer_pool1x1 = tf.layers.MaxPooling2D(pool_size=[1, 1], strides=1)
+layer_pool2x2 = tf.layers.MaxPooling2D(pool_size=[2, 2], strides=2)
+layer_pool3x3 = tf.layers.MaxPooling2D(pool_size=[3, 3], strides=3)
+layer_cnn1 = tf.layers.Conv2D(
+    filters=128,
+    kernel_size=[3, 3],
+    padding="same",
+    activation=tf.nn.relu,
+    # strides=[2,2]
+)
+layer_cnn2 = tf.layers.Conv2D(
+    filters=1024,
+    kernel_size=[2, 2],
+    padding="same",
+    activation=tf.nn.relu,
+    # strides=[2,2]
+)
 layer_flatten = tf.layers.Flatten()
 layer_fc0 = tf.layers.Dense(dim_hidden, activation=tf.nn.relu)
-layer_dropout = tf.layers.Dropout(rate=0.75)  # dropout rate is 0.75. Retain 0.25
+layer_dropout = tf.layers.Dropout(rate=.75)  # dropout rate is 0.75. Retain 0.25
 layer_fc1 = tf.layers.Dense(10, activation=None)  # 1
 
 
 # forward propagation
 def prediction(X, training):
+    # Instantiate
     values = tf.constant(X)
-    values = layer_pool4x4(values)  # this must be the first layer
+    # First Layer from Prof
+    values = layer_pool4x4(values)
+    # Conv layer 1
     values = layer_cnn0(values)
-    # values = layer_cnn1(values)
-    values = layer_pool2x2(values)
+    # Conv layer 2
+    values = layer_cnn1(values)
+    # Conv layer 3
+    values = layer_cnn2(values)
+    # Fully connected
     values = layer_flatten(values)
     values = layer_fc0(values)
     values = layer_dropout(values, training=training)
@@ -66,14 +91,13 @@ X_validation = X_validation.reshape([-1, 28, 28, 1])
 def v_binary_accuracy():
     return (binary_accuracy(X_validation, y_validation))
 
-
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
-batch_size = 50
-iters = 2000
+batch_size = 200
+iters = 500
 
 for i in range(iters):
     X, y = mnist.train.next_batch(batch_size)
     X = X.reshape([-1, 28, 28, 1])
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
     optimizer.minimize(lambda: loss(X, y, True))
 
     if i % 100 == 0:
